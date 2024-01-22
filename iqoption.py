28459ae9efc4f23e60831872856ef4c10b157398
Anderson-Andre-P <eleu.ander@gmail.com>
2024-01-22 09:58:18 -0300

    fix: correcting flow when user tries to buy using more money than they have

--- iqoption.py
+++ iqoption.py
@@ -6,19 +6,17 @@ def connect_iq_option(email, password, account_type):
     check, reason = API.connect()
 
     if check:
-        print('Connected successfully!\n')
         return API, account_type, True
-    else:
-        if reason == '{"code":"invalid_credentials","message":"You entered the wrong credentials. Please ensure that your login/password is correct."}':
-            print('Invalid email or password.')
-        else:
-            print('\nConnection timeout...')
-            print(f'{reason}')
-        return None, None, False
+    # else:
+        # if reason == '{"code":"invalid_credentials","message":"You entered the wrong credentials. Please ensure that your login/password is correct."}':
+            # print('Invalid email or password.')
+            # print(f'{reason}')
+        # else:
+        #     print('\nConnection timeout...')
+        #     print(f'{reason}')
+        # return None, None, False
 
 def purchase_with_gale(API, marker, input_value, direction, expiration, type, gale_quantity, gale_multiplier, bot, chat_id):
-    print(f"Starting Gale with {gale_quantity} attempts and multiplier {gale_multiplier}")
-
     for attempt in range(gale_quantity):
         if type == 'digital':
             check, id = API.buy_digital_spot_v2(marker, input_value, direction, expiration)
@@ -26,8 +24,6 @@ def purchase_with_gale(API, marker, input_value, direction, expiration, type, ga
             check, id = API.buy(input_value, marker, direction, expiration)
 
         if check:
-            print(f"Purchase made, ID: {id}")
-
             while True:
                 time.sleep(0.1)
                 status, result = API.check_win_digital_v2(id) if type == 'digital' else API.check_win_v4(id)
@@ -40,12 +36,11 @@ def purchase_with_gale(API, marker, input_value, direction, expiration, type, ga
                     else:
                         loss_amount = abs(result)
                         loss_info = f"❌ Operation With Loss ❌\nValue: -${round(loss_amount, 2)}\nRemaining Attempts: {gale_quantity - attempt - 1}\nMultiplier: {gale_multiplier}"
-                        print(loss_info)
                         bot.send_message(chat_id, loss_info)
                         break
 
         else:
-            print(f'Error opening order, {id}.')
+            return {"result": "Error opening order.\n\nThe position value is greater than allowed in the configuration.\n\nUse the /reset_purchase command and start the purchase process again with valid data."}
 
         input_value *= gale_multiplier
 
