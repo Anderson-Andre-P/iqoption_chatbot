919ad00f143a6418867d8bc66e14094d39b68c07
Anderson-Andre-P <eleu.ander@gmail.com>
2024-01-29 09:50:15 -0300

    fix config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+# import os
 
-load_dotenv()
+# load_dotenv()
 API_TOKEN = '6329000949:AAHr6lu_W_U_fPi9dMnYv4mWDIXTxpJjMOw'
