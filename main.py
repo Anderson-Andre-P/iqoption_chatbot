6f97130803edba46f36da7e2abf3d3f65ad73aeb
Anderson-Andre-P <eleu.ander@gmail.com>
2024-01-29 10:57:16 -0300

    modify main

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from flask import Flask
 import telegram_bot
 import config
-import threading
 
 app = Flask(__name__)
 
-def run_bot():
-    telegram_bot.bot.polling(none_stop=True)
-
 if __name__ == '__main__':
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
+    telegram_bot.bot.polling(none_stop=True)
     app.run(debug=True)
-
